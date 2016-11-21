f9aea0da88bb052cf61209bc9860c1b9d8b63885
matt-hfc <iasKor12>
2016-11-20 22:15:44 -0500

    Small tweak

--- src/main/java/util/factory/CapabilitiesFactory.java
+++ src/main/java/util/factory/CapabilitiesFactory.java
@@ -24,7 +24,7 @@ public class CapabilitiesFactory {
                 try {
 
 
-                    desiredCapabilities.setCapability(field.getName(), testEnvironment.getClass().getField(field.getName()).get(testEnvironment).toString());
+                    desiredCapabilities.setCapability(field.getName(), testEnvironment.getClass().getField(field.getName()).get(testEnvironment));
                 } catch (Exception e) {}
             }
             desiredCapabilitiesList.add(desiredCapabilities);
