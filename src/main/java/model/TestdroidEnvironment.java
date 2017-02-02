cf6d25ce8d752bab19d204add9e4204125edd7f7
matt-hfc <iasKor12>
2017-02-02 14:05:23 -0500

    Added required devicename field

--- src/main/java/model/TestdroidEnvironment.java
+++ src/main/java/model/TestdroidEnvironment.java
@@ -12,6 +12,7 @@ public class TestdroidEnvironment extends TestEnvironment {
     public String testdroid_testrun;
     public String testdroid_device;
     public String testdroid_target;
+    public String deviceName;
 
     public String getTestdroid_target() {
         return testdroid_target;
