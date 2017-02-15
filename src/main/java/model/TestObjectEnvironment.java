1fbe17b02aa59e0da68274b5359b36ab27be3201
matt-hfc <iasKor12>
2017-02-15 12:12:33 -0500

    Fixed wrong name of capability

--- src/main/java/model/TestObjectEnvironment.java
+++ src/main/java/model/TestObjectEnvironment.java
@@ -7,7 +7,7 @@ package model;
 public class TestObjectEnvironment extends TestEnvironment {
 
     public String testobject_device_name;
-    public String testobject_api_key;
+    public String testdroid_apiKey;
     public String testobject_appium_version;
 
     public String getTestobject_appium_version() {
@@ -18,7 +18,7 @@ public class TestObjectEnvironment extends TestEnvironment {
         return testobject_device_name;
     }
 
-    public String getTestobject_api_key() {
-        return testobject_api_key;
+    public String getTestdroid_apiKey() {
+        return testdroid_apiKey;
     }
 }
